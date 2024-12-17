38ccbcc6bff3ad91b80af5b7eddf47b683dbbf69
erascu <e.rashku@gmail.com>
2024-12-17 08:48:44 +0000

    add isLoading logic

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import ArticleBlock from "./ArticleBlock";
 
-function Articles({ articles }) {
+function Articles({ articles, isLoading }) {
   return (
     <>
       <div className="content-block">
         <h2 className="content-title">Articles</h2>
       </div>
       <ul>
-        {articles.map((article) => (
-          <li key={article.article_id}>
-            <ArticleBlock article={article} />
-          </li>
-        ))}
+        {isLoading
+          ? "Loading..."
+          : articles.map((article) => (
+              <li key={article.article_id}>
+                <ArticleBlock article={article} />
+              </li>
+            ))}
       </ul>
     </>
   );

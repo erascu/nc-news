d6c0e181439b535329e65c8dba95106663ad4f21
erascu <e.rashku@gmail.com>
2024-12-20 14:41:19 +0000

    create NotFound component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,12 +1,27 @@
-function NotFound() {
+import { useNavigate } from "react-router";
+import { customErrorMsgs } from "../utils/customErrorMsgs";
+
+function NotFound({ costumError }) {
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
+  const customMsg =
+    costumError &&
+    customErrorMsgs.hasOwnProperty(costumError.code) &&
+    customErrorMsgs[costumError.code];
+
   return (
-    <div className="notfound-block">
+    <div className="notfound-block no-scroll">
       <div className="notfound-body">
-        <button>Close</button>
-        <h2>404</h2>
-        <h3>Not Found</h3>
+        <button onClick={handleClick}>Back to main</button>
+        <h2>{costumError ? costumError.code : "404"}</h2>
+        <h3>{costumError ? customMsg : "Not found"}</h3>
         <p>
-          We couldn't process your vote at the moment. Please try again later.
+          {costumError ? costumError.message : "Oops! Something went wrong!"}
         </p>
       </div>
     </div>
